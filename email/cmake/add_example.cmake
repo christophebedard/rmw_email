0f746b6c1aca2db4dd7d069258f0e36fc599c7a2
Christophe Bedard <bedard.christophe@gmail.com>
2020-07-19 14:40:27 -0400

    Extract CMake macro for setting up examples and clean up file
    This extracts a simple CMake macro to perform the setup for the
    examples. It also cleans up the CMakeLists.txt file a bit and does some
    minor fixes.
    
    Signed-off-by: Christophe Bedard <bedard.christophe@gmail.com>

---
+++ email/cmake/add_example.cmake
@@ -0,0 +1,34 @@
+# Copyright 2020 Christophe Bedard
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# Add executable, target project lib and install an example.
+#
+# :param name: the name of the example
+#   (name of the .cpp file under the example/ directory;
+#   will be used as the executable name)
+# :type name: string
+#
+macro(add_example name)
+  add_executable(${name}
+    example/${name}.cpp
+  )
+  target_link_libraries(${name}
+    ${PROJECT_NAME}
+  )
+  install(TARGETS
+    ${name}
+    DESTINATION lib/${PROJECT_NAME}
+  )
+endmacro()
