e20ecb24f7e4fb0c3106c20e17b7f3268daa5722
Christophe Bedard <bedard.christophe@gmail.com>
2021-04-06 20:01:01 -0400

    Allow writing logs to file using EMAIL_LOG_FILE env var
    Signed-off-by: Christophe Bedard <bedard.christophe@gmail.com>

--- email/src/log.cpp
+++ email/src/log.cpp
@@ -15,7 +15,9 @@
 #include <memory>
 #include <mutex>
 #include <string>
+#include <vector>
 
+#include "spdlog/sinks/basic_file_sink.h"
 #include "spdlog/sinks/stdout_color_sinks.h"
 #include "spdlog/spdlog.h"
 
@@ -29,6 +31,8 @@ namespace log
 
 static std::mutex logger_mutex;
 static std::shared_ptr<spdlog::logger> root_logger = nullptr;
+static spdlog::sink_ptr sink_console = nullptr;
+static constexpr const char * ENV_VAR_LOG_FILE = "EMAIL_LOG_FILE";
 static constexpr const char * ENV_VAR_LOG_LEVEL = "EMAIL_LOG_LEVEL";
 static constexpr const char * ENV_VAR_LOG_LEVEL_DEFAULT = "info";
 
@@ -82,14 +86,35 @@ init()
   if (nullptr != root_logger) {
     return;
   }
-  root_logger = spdlog::stdout_color_mt("root");
+
+  // Create a console sink
+  std::vector<spdlog::sink_ptr> sinks = {};
+  sink_console = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+  sinks.push_back(sink_console);
+
+  // Create a file sink if a log file path was provided
+  const std::string log_file = utils::get_env_var(ENV_VAR_LOG_FILE);
+  const bool log_to_file = !log_file.empty();
+  if (log_to_file) {
+    // Set to debug so that everything gets logged to file
+    auto sink_file = std::make_shared<spdlog::sinks::basic_file_sink_mt>(log_file, false);
+    sink_file->set_level(spdlog::level::debug);
+    sinks.push_back(sink_file);
+    // TODO(christophebedard) flush periodically using spdlog::flush_every()?
+  }
+
+  root_logger = std::make_shared<spdlog::logger>("root", sinks.begin(), sinks.end());
+  root_logger->set_level(spdlog::level::debug);
+  spdlog::register_logger(root_logger);
+  root_logger->debug("logging to file: " + std::string(log_to_file ? "true" : "false"));
 }
 
 void
 set_level(const Level & level)
 {
   std::lock_guard<std::mutex> lock(logger_mutex);
-  root_logger->set_level(level_to_spdlog(level));
+  // Only change log level for console sink
+  sink_console->set_level(level_to_spdlog(level));
 }
 
 void
@@ -99,6 +124,7 @@ set_level_from_env()
     ENV_VAR_LOG_LEVEL,
     ENV_VAR_LOG_LEVEL_DEFAULT);
   set_level(level_from_string(env_log_level));
+  root_logger->debug("logging level set to: " + env_log_level);
 }
 
 std::shared_ptr<spdlog::logger>
