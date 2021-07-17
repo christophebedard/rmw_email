928e3e5358fc5b371e4afdb1ffbaaee1bea3422c
Christophe Bedard <bedard.christophe@gmail.com>
2021-07-17 19:37:07 -0400

    Complete rmw guard condition function implementations (#151)
    Signed-off-by: Christophe Bedard <bedard.christophe@gmail.com>

--- rmw_email_cpp/src/rmw_guard_condition.cpp
+++ rmw_email_cpp/src/rmw_guard_condition.cpp
@@ -12,28 +12,46 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// #include "rcpputils/scope_exit.hpp"
-// #include "rmw/allocators.h"
-// #include "rmw/error_handling.h"
 #include "rmw/impl/cpp/macros.hpp"
 #include "rmw/rmw.h"
 
+#include "email/guard_condition.hpp"
+
 #include "rmw_email_cpp/identifier.hpp"
-// #include "rmw_email_cpp/macros.hpp"
-// #include "rmw_email_cpp/types.hpp"
+#include "rmw_email_cpp/types.hpp"
 
 static rmw_guard_condition_t * _create_guard_condition()
 {
-  rmw_guard_condition_t * guard_condition = new rmw_guard_condition_t;
+  auto email_guard_condition = new (std::nothrow) email::GuardCondition();
+  if (nullptr == email_guard_condition) {
+    RMW_SET_ERROR_MSG("failed to allocate guard condition impl");
+    return nullptr;
+  }
+
+  auto rmw_email_guard_condition = new (std::nothrow) rmw_email_guard_condition_t;
+  if (nullptr == email_guard_condition) {
+    RMW_SET_ERROR_MSG("failed to allocate rmw guard condition");
+    return nullptr;
+  }
+  rmw_email_guard_condition->email_guard_condition = email_guard_condition;
+
+  rmw_guard_condition_t * guard_condition = new (std::nothrow) rmw_guard_condition_t;
+  if (nullptr == guard_condition) {
+    RMW_SET_ERROR_MSG("failed to allocate guard condition");
+    return nullptr;
+  }
   guard_condition->implementation_identifier = email_identifier;
-  // TODO(christophebedard) figure out
-  guard_condition->data = nullptr;
+  guard_condition->data = rmw_email_guard_condition;
   return guard_condition;
 }
 
 static rmw_ret_t _destroy_guard_condition(rmw_guard_condition_t * guard_condition)
 {
-  // delete guard_condition->data;
+  auto rmw_email_guard_condition = static_cast<rmw_email_guard_condition_t *>(
+    guard_condition->data);
+  auto email_guard_condition = rmw_email_guard_condition->email_guard_condition;
+  delete email_guard_condition;
+  delete rmw_email_guard_condition;
   delete guard_condition;
   return RMW_RET_OK;
 }
@@ -59,7 +77,9 @@ extern "C" rmw_ret_t rmw_trigger_guard_condition(const rmw_guard_condition_t * g
     email_identifier,
     return RMW_RET_INCORRECT_RMW_IMPLEMENTATION);
 
-  // TODO(christophebedard) figure out
-  // guard_condition->data
+  auto rmw_email_guard_condition = static_cast<rmw_email_guard_condition_t *>(
+    guard_condition->data);
+  auto email_guard_condition = rmw_email_guard_condition->email_guard_condition;
+  email_guard_condition->trigger();
   return RMW_RET_OK;
 }
