d71af734850915a1053621a6788aa52f70bd90ef
Christophe Bedard <bedard.christophe@gmail.com>
2021-07-19 09:47:26 -0400

    Convert GID to string once on construction, add GID-specific test (#161)
    Signed-off-by: Christophe Bedard <bedard.christophe@gmail.com>

---
+++ email/test/test_gid.cpp
@@ -0,0 +1,41 @@
+// Copyright 2021 Christophe Bedard
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <gtest/gtest.h>
+
+#include "email/gid.hpp"
+
+TEST(TestGid, gid) {
+  email::Gid gid1;
+  email::Gid gid2;
+  email::Gid gid3;
+
+  // May be overkill
+  EXPECT_NE(0u, gid1.value());
+  EXPECT_NE(0u, gid2.value());
+  EXPECT_NE(0u, gid3.value());
+  EXPECT_NE(gid1.value(), gid2.value());
+  EXPECT_NE(gid1.value(), gid3.value());
+  EXPECT_NE(gid2.value(), gid3.value());
+  EXPECT_FALSE(gid1.as_string().empty());
+  EXPECT_FALSE(gid2.as_string().empty());
+  EXPECT_FALSE(gid3.as_string().empty());
+  EXPECT_STRNE(gid1.as_string().c_str(), gid2.as_string().c_str());
+  EXPECT_STRNE(gid1.as_string().c_str(), gid3.as_string().c_str());
+  EXPECT_STRNE(gid2.as_string().c_str(), gid3.as_string().c_str());
+
+  // Check sequentiality (wow that's a real word)
+  EXPECT_EQ(1u, gid2.value() - gid1.value());
+  EXPECT_EQ(1u, gid3.value() - gid2.value());
+}
